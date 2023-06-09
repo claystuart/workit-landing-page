357289049267f5c6541acec5200e72c072905eab
Clay Stuart <clay.stuart@gmail.com>
2023-06-09 15:38:06 -0400

    Styled phone image.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import Header from './Header';
 import SellingPoints from './SellingPoints';
 import Footer from './Footer';
+import phone from './assets/images/image-phone.webp';
+import "./Phone.css";
 
 export default function App() {
 
   return (
-    <>
+    <div className='container'>
       <Header />
+      <img src={phone} className='img__phone' alt="phone" />
       <SellingPoints />
       <Footer />
-    </>
+    </div>
   )
 }
